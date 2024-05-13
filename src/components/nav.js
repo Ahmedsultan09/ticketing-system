6f550641c17195c9f985f000817b14adc22f39f2
Bigdata acc <as11bs22@gmail.com>
2024-05-13 10:35:01 +0300

    edited some routes

--- src/components/nav.js
+++ src/components/nav.js
@@ -107,7 +107,7 @@ export default function Example() {
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
-                          <a
+                          <Direct
                             to={navigation.href}
                             className={classNames(
                               active ? "bg-gray-100" : "",
@@ -115,12 +115,12 @@ export default function Example() {
                             )}
                           >
                             Your Profile
-                          </a>
+                          </Direct>
                         )}
                       </Menu.Item>
                       <Menu.Item>
                         {({ active }) => (
-                          <a
+                          <Direct
                             href="#"
                             className={classNames(
                               active ? "bg-gray-100" : "",
@@ -128,12 +128,12 @@ export default function Example() {
                             )}
                           >
                             Settings
-                          </a>
+                          </Direct>
                         )}
                       </Menu.Item>
                       <Menu.Item>
                         {({ active }) => (
-                          <a
+                          <Direct
                             href="#"
                             className={classNames(
                               active ? "bg-gray-100" : "",
@@ -141,7 +141,7 @@ export default function Example() {
                             )}
                           >
                             Sign out
-                          </a>
+                          </Direct>
                         )}
                       </Menu.Item>
                     </Menu.Items>
